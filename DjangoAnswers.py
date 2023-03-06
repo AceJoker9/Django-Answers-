965c01fbc90b96ca69c7dede2ecc21e7c5bfa2d8
unknown <neilmagic25@gmail.com>
2023-03-06 03:34:35 -0500

    Added problems 3 and 4

--- DjangoAnswers.py
+++ DjangoAnswers.py
@@ -1,3 +1,32 @@
 # The First problem was the toughest by far, coding the "DESC" is still the biggest hurdle to jump
 #This is what I got for problem number 1 
 
+ x = Instructor.objects.filter(Instructor.hire_date) 
+  for y in x:
+    if x <= '2010-01-01':
+      print (f'Full Name: {Instructor.first_name} {Instructor.last_name} Hire Date: {Instructor.hire_date}')
+
+
+
+# Problem #2
+
+teacher = Instructor.object.get(pk=2)
+  shared_course = Course.objects.filter(Course.instructor(pk=2))
+  print(
+              f' Instructor Name: {teacher}, Courses: {shared_course}')
+
+
+#Problem #3
+
+  kids = Student.objects.count
+  classes = Course.objects.count
+  professors = Instructor.objects.count
+
+  print
+  ("Student Count: {kids}, Courses Count: {classes}, Instructors Count: {professors}")
+
+#This is was a little tricky, cause the variables, dont light up showing that they belong to anything on the Django Python file. 
+
+
+#Problem #4
+
